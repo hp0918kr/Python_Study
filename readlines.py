a4857607e178fdd75c9d01f41e41da0100fb4663
LeeHwapyeong <hp0918kr@gmail.com>
2023-09-26 11:11:13 +0900

    2023.09.26 파이썬 수업

---
+++ readlines.py
@@ -0,0 +1,7 @@
+#readlines.py
+f = open("C:\python\새파일.txt", 'r')
+lines = f.readlines()
+for line in lines:
+    print(line)
+f.close()
+
---
+++ sys1.py
@@ -0,0 +1,5 @@
+#sys1.py
+import sys
+args = sys.argv[1:]
+for i in args:
+    print(i)
